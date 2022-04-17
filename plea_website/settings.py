e6fca309600175a11667da2742b7cda33f43e3d3
Stephen Jimenez <bacchus_16@hotmail.com>
2022-04-17 13:08:26 -0400

    commit

--- plea_website/settings.py
+++ plea_website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v%ixqs3d74v35!8(1xj$pub-b%2(+j$l-nsb#-rl@$9lh&q)o$'
+SECRET_KEY = 'v%ixqs3d76874562aslkjh(1xj$pub-b%2(+j$l-nsb#-rl@$9lh&q)o$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
